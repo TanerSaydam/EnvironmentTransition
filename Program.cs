d1de172a7e2ad5da60f156c9d1c0a3bd933f8cb5
Taner Saydam <taner_and_you@hotmail.com>
2025-06-08 18:42:38 +0300

    Development ortamı ekleyelim

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var env = builder.Environment.EnvironmentName; // Development // Production
+
 builder.Configuration
-    .AddJsonFile(
-    "appsettings.json",
-    optional: true,
-    reloadOnChange: false
-    );
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+    .AddJsonFile("appsettings." + env + ".json", optional: true, reloadOnChange: false)
+    ;
 
-var key = builder.Configuration.GetSection("Key").Value;
+var con = builder.Configuration.GetSection("SqlCon").Value;
 
-Console.WriteLine(key);
+Console.WriteLine(con);
 
 var app = builder.Build();
 
