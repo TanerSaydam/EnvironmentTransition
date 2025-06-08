e67a76d80f6bcfd563ce9afb0004321dd12e4e64
Taner Saydam <taner_and_you@hotmail.com>
2025-06-08 19:16:04 +0300

    Publish ortamı için

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var env = builder.Environment.EnvironmentName; // Development // Production
 
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
-    .AddJsonFile("appsettings." + env + ".json", optional: true, reloadOnChange: false)
+    //.AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: false)
     ;
 
 var con = builder.Configuration.GetSection("SqlCon").Value;
